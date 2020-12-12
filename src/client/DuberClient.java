747e9a3a94eb8a40cc3aacb75932f73554e9a7c3
Void-Foxx <Void-Foxx@users.noreply.github.com>
2020-12-12 13:04:51 -0500

    Created loginPanel

--- src/client/DuberClient.java
+++ src/client/DuberClient.java
@@ -10,6 +10,8 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.plaf.DimensionUIResource;
 
+import messages.DoLogin;
+
 import java.io.*;
 import java.net.*;
 import java.awt.*;
@@ -19,32 +21,39 @@ import java.awt.event.*;
 class ChatClient {
   
   private JButton sendButton, clearButton, friendButtonList;
-  //private JButton[] friendButtonList;
   private JTextField typeField;
-  private JTextArea msgArea;  
-  private JPanel southPanel, westPanel;
+  private JTextArea msgArea, usernameField;  
+  private JPasswordField passwordField;
+  private JPanel southPanel, westPanel, eastPanel;
+  private JPanel southEastPanel, southWestPanel;
   private Socket mySocket; //socket for connection
-  private BufferedReader input; //reader for network stream
-  private PrintWriter output;  //printwriter for network output
+  private ObjectInputStream input; //Stream for network input
+  private ObjectOutputStream output;
   private boolean running = true; //thread status via boolean
   private String[] names;
+
   
-  public static void main(String[] args) { 
-    new ChatClient().go();
-  }
     
   public void go() {
-    JFrame window = new JFrame("DuberChat");
-    westPanel = new JPanel();
-    southPanel = new JPanel();
-    westPanel.setLayout(new GridLayout(0,1));
-    southPanel.setLayout(new GridLayout(2,0));
-    
-    sendButton = new JButton("SEND");
-    sendButton.addActionListener(new SendButtonListener());
-    clearButton = new JButton("QUIT");
-    clearButton.addActionListener(new QuitButtonListener());
+    // call a method that connects to the server 
+    connect("127.0.0.1",5000);
 
+    JFrame window = new JFrame("DuberChat");
+    
+    //login screen
+    LoginPanel loginPanel = new LoginPanel(input,output);
+
+    window.add(BorderLayout.CENTER,loginPanel.getPanel());
+    window.setSize(200,200);
+    window.setVisible(true);
+    boolean login = true;
+    while(login){
+
+    }
+
+    window.setVisible(false);
+    window.setSize(1600,900);
+    //account information retrival
     names = new String[5];
     names[0] = "ryan";
     names[1] = "kevin";
@@ -59,6 +68,23 @@ class ChatClient {
       friendButtonList.addActionListener(new SendButtonListener());//allows the button to be interacted with
       westPanel.add(friendButtonList);//adds the new buttom to the list on the left
     }
+    //chat gui
+    westPanel = new JPanel();
+    eastPanel = new JPanel();
+    southPanel = new JPanel();
+    southEastPanel = new JPanel();
+    southWestPanel = new JPanel();
+    
+    westPanel.setLayout(new GridLayout(0,1));
+    southPanel.setLayout(new GridLayout(0,2));
+    eastPanel.setLayout(new GridLayout(0,1));
+    southEastPanel.setLayout(new GridLayout(0,1));
+    southWestPanel.setLayout(new GridLayout(0,1));
+
+    sendButton = new JButton("SEND");
+    sendButton.addActionListener(new SendButtonListener());
+    clearButton = new JButton("QUIT");
+    clearButton.addActionListener(new QuitButtonListener());
     
     JLabel errorLabel = new JLabel("");
     
@@ -66,24 +92,31 @@ class ChatClient {
     
     msgArea = new JTextArea();
     
-    southPanel.add(typeField);
-    southPanel.add(sendButton);
-    southPanel.add(errorLabel);
-    southPanel.add(clearButton);
+    southWestPanel.add(typeField);
+    southEastPanel.add(sendButton);
+    southEastPanel.add(clearButton);
+    //southPanel.add(errorLabel);
+    southPanel.add(southWestPanel);
+    southPanel.add(southEastPanel);
+    //southPanel.setMaximumSize(new Dimension(900,100));
     
+    //eastPanel.add(msgArea);
+    //eastPanel.add(southPanel);
+
+    //window.add(BorderLayout.CENTER,eastPanel);
     window.add(BorderLayout.CENTER,msgArea);
     window.add(BorderLayout.WEST,westPanel);
     window.add(BorderLayout.SOUTH,southPanel);
     
+    
     window.setSize(1600,900);
     window.setVisible(true);
     
-    // call a method that connects to the server 
-    connect("127.0.0.1",5000);
+    
     // after connecting loop and keep appending[.append()] to the JTextArea
     
     readMessagesFromServer();
-    
+    window.dispose();
   }
   
   //Attempts to connect to the server and creates the socket and streams
@@ -93,13 +126,10 @@ class ChatClient {
     try {
       mySocket = new Socket(ip, port); //attempt socket connection (local address). This will wait until a connection is made
 
-      ObjectInputStream in = new ObjectInputStream(mySocket.getInputStream()); //Stream for network input
-      ObjectOutputStream out = new ObjectOutputStream(mySocket.getOutputStream()); //Stream for network output
+      ObjectInputStream input = new ObjectInputStream(mySocket.getInputStream()); //Stream for network input
+      ObjectOutputStream output = new ObjectOutputStream(mySocket.getOutputStream()); //Stream for network output
 
-      InputStreamReader stream1= new InputStreamReader(mySocket.getInputStream()); //Stream for network input
-      input = new BufferedReader(stream1);     
-      output = new PrintWriter(mySocket.getOutputStream()); //assign printwriter to network stream
-      
+      ObjectInputStream ping = new ObjectInputStream(mySocket.getInputStream());//pings from server to update for messages
     } catch (IOException e) {  //connection error occured
       System.out.println("Connection to Server Failed");
       e.printStackTrace();
@@ -108,52 +138,61 @@ class ChatClient {
     System.out.println("Connection made.");
     return mySocket;
   }
+  public void login(){
+    
+  }
   
   //Starts a loop waiting for server input and then displays it on the textArea
   public void readMessagesFromServer() { 
-    
-  while(running) {  // loop unit a message is received
+
+    /*
+    while(running) {  // loop unit a message is received
       try {
+        input.readObject();
         
-        if (input.ready()) { //check for an incoming messge
-          String msg;          
-          msg = input.readLine(); //read the message
-          System.out.println("received: "+msg);
-          msgArea.append(msg+"\n");
-        }
         
       }catch (IOException e) { 
         System.out.println("Failed to receive msg from the server");
         e.printStackTrace();
       }
-}
-     try {  //after leaving the main loop we need to close all the sockets
+    }
+    */
+
+    try {  //after leaving the main loop we need to close all the sockets
       input.close();
       output.close();
       mySocket.close();
     }catch (Exception e) { 
       System.out.println("Failed to close socket");
     }
-  
   }
   //****** Inner Classes for Action Listeners ****
   
     // send - send msg to server (also flush), then clear the JTextField
     class SendButtonListener implements ActionListener { 
-     public void actionPerformed(ActionEvent event)  {
-        //Send a message to the client
-       output.println(typeField.getText());
-       output.flush();             
-       typeField.setText(""); 
-     }     
-   }
+      public void actionPerformed(ActionEvent event)  {
+        //Send a message to the client            
+        typeField.setText(""); 
+      }     
+    }
 
   // QuitButtonListener - Quit the program
-     class QuitButtonListener implements ActionListener { 
+    class QuitButtonListener implements ActionListener { 
      public void actionPerformed(ActionEvent event)  {
        running=false;
      }     
-   }
+    }
 
-  
+  class FriendButtonListener implements ActionListener { 
+    private int friendID;
+    public void actionPerformed(ActionEvent event)  {
+      
+    }
+    public void setFriendID(int ID){
+      this.friendID = ID;
+    }
+  }
+  public static void main(String[] args) { 
+    new ChatClient().go();
+  }
 }
